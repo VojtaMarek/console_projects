94ade80ea2aad1a634bbfb086b6d918cef667934
VojtaMarek <vojtama@gmail.com>
2022-10-25 11:41:39 +0200

    first commit, deployed as a beta version

---
+++ probabilityDice/dice.py
@@ -0,0 +1,102 @@
+# :: Through dice a number of times and try the so-called 'Monte Carlo Method' or 'Monte Carlo sampling'.
+# :: This gives a proven and widely recognized alternative for statistical calculations and may
+# :: save you some brain cells :)
+
+import random
+# Enter: styletext('text', S.FAIL), by default color = WARNING
+import style as S
+from style import styletext
+
+def inputUser(string_atr, default="1"):
+    try:
+        print("{:<30s}".format(string_atr) + f"({default} is set by default)")
+        atr = int(input("{:<31s}".format('')).strip())
+    except:
+        atr = default
+        #print("{:<20s}".format("") + f"(default: {atr})")
+
+    return int(atr)
+
+
+def printHeader():
+    print("\nRoll dice simulation to avoid statistical calculations by so-called 'Monte Carlo method'.\nFirst, choose one of the game types to through dice:\n")
+
+
+    print("\t1 -> All dice with the same side value")
+    print("\t2 -> All dice values equal to 1")
+    # Space to add more game options here eventually
+    print('')
+
+    game_type = inputUser("Game type:",)
+    dice_num = inputUser("Number of dice:", "2")
+    sides_num = inputUser("A dice sides:", "6")
+    times_num = inputUser("Test repetitions:", "1000000")
+
+    return game_type, dice_num, sides_num, times_num
+
+def decimalsShown(number):
+    count = 0
+    multiplier = 1.0        # tady jsem měl int (floatem se to vyřešilo) a házelo mi to u 10mil pokusů 'OverflowError: int too large to convert to float', proč?
+    while True:
+        if number * multiplier > 1:
+            return count
+        multiplier *= 10
+        count += 1
+
+def main():
+    game_type, dice_num, sides_num, times_num = printHeader()
+
+    times_num_backup = times_num
+    # get it roll
+    dice_records = []
+    for _ in range(times_num):
+        dice_rolls = []
+        for _ in range(dice_num):
+            dice_roll = random.randint(1, sides_num)
+            dice_rolls.append(dice_roll)
+            # print(dice_rolls)
+        dice_records.append(dice_rolls)
+        #times_num -= 1
+
+    # print(dice_records)
+    # print(f'Game type {game_type}')
+    count = 0
+    print(f"\nGame type {game_type}: ", end='')
+
+    # game_type 1:  All dice with the same side value
+    if game_type == 1:
+        for rolls in dice_records:
+            flag = False
+            for roll in rolls:
+                if roll != rolls[-1]:
+                    flag = False
+                    break
+                else:
+                    flag = True
+            if flag:
+                count += 1
+
+    # game_type 2: All dice values equal to 6
+    if game_type == 2:
+        for rolls in dice_records:
+            flag = False
+            for roll in rolls:
+                if roll != 6:
+                    flag = False
+                    break
+                else:
+                    flag = True
+            if flag:
+                count += 1
+
+    
+    
+    resultNum = float(count / times_num_backup)
+    decimals = decimalsShown(resultNum)
+    
+    print(styletext("{:<20s}".format(f"There are {count} ocurence(s) from {times_num_backup}. ")), end='')
+    print("That corresponds to {:.{dec}%}".format(resultNum, dec = decimals), end='\n\n')
+
+
+if __name__ == "__main__":
+    main()
---
+++ probabilityDice/style.py
@@ -0,0 +1,35 @@
+
+
+#reset style
+RESET = '\033[0m'
+#text styles
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+#others
+UNDERLINE = '\033[4m'
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKCYAN = '\033[96m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+
+@staticmethod
+def styletext(text, color = WARNING):
+    """change style of text, see global variables""" 
+    return(f'{color}{text}{RESET}')
+
+if __name__ == '__main__':
+    styles_list = [RESET, BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, UNDERLINE, HEADER, OKBLUE, OKCYAN, OKGREEN, WARNING, FAIL, ENDC, BOLD, UNDERLINE]
+    for i in styles_list: print(styletext('Hello World!', i))
+    print('Hello World!')
